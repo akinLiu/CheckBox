0ef3dec2b7e0428ace7b87c405a4720e10c9f486
akinLiu <hengxin196@gmail.com>
2012-04-23 22:07:42 +0800

    Add AKCheckBox

---
+++ AKCheckBox/AKCheckBox.m
@@ -0,0 +1,31 @@
+//
+//  AKCheckBox.m
+//  CheckBoxDemo
+//
+//  Created by akin liu on 12-4-23.
+//  Copyright (c) 2012年 hengxinsoft.com. All rights reserved.
+//
+
+#import "AKCheckBox.h"
+
+@implementation AKCheckBox
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
